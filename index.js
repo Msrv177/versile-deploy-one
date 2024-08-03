9b061206c89778f7188107c05d04eddafe10a223
Msrv177 <msrvasupalli177@gmail.com>
2024-08-03 14:17:20 +0530

    after some test

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express() 
 const port = 3000 
 
+app.listen(port,()=>{
+    console.log(` running at port - ${port}`);    
+})
+
 app.get('/',(req,res)=>{
     res.send(`<h1> heloo MSR V</h1>`)
 })
+app.get('/msr',(req,res)=>{
+    res.send(`<h1> heloo madhusudhana rao V</h1>`)
+})
 
-app.listen(port,()=>{
-    console.log(` running at port - ${port}`);
-    
+
+app.get('/:id?',(req,res)=>{
+    let id = req.params.id ;
+    res.send(`<h1> ${id} -  madhusudhana rao V</h1>`)
 })
